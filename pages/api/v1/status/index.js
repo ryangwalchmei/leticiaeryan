6fcecb1213efe219c45ab32c6c4fef8b7d634fb9
Ryan Gwalchmei <ryan.gwalchmei@outlook.com.br>
2025-01-28 15:21:34 -0300

    endpoint `terminate /api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,35 @@
-import database from "infra/database";
+import database from "infra/database.js";
 
-async function status(req, res) {
-  const result = await database.query("SELECT 1+1 as sum;");
+const status = async (request, response) => {
+  const updated_at = new Date().toISOString();
+  const databaseName = process.env.POSTGRES_DB;
 
-  console.log(result.rows);
+  const databaseVersionResult = await database.query(`SHOW server_version;`);
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  res.status(200).json(result.rows);
-}
+  const databaseMaxConnectionsResult = await database.query(
+    `SHOW max_connections;`,
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datName = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: Number(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
+};
 
 export default status;
