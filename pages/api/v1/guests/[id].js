fef9f4406ef234d6998f3fa7bb707d9e7800ec4c
Ryan Gwalchmei <ryan.gwalchmei@outlook.com.br>
2025-03-04 15:09:18 -0300

    refactor: fixes general implementation of `guest` endpoint and makes tests cover more scenarios

--- pages/api/v1/guests/[id].js
+++ pages/api/v1/guests/[id].js
@@ -3,12 +3,16 @@ const guests = guestsFactory();
 
 export default function Guests(request, response) {
   try {
-    if (request.method === "GET") {
-      return getHandler(request, response);
-    } else if (request.method === "PUT") {
-      return putHandler(request, response);
-    } else if (request.method === "DELETE") {
-      return deleteHandler(request, response);
+    switch (request.method) {
+      case "GET":
+        return getHandler(request, response);
+      case "PUT":
+        return putHandler(request, response);
+      case "DELETE":
+        return deleteHandler(request, response);
+      default:
+        response.setHeader("Allow", ["GET", "PUT", "DELETE"]);
+        return response.status(405).end(`Method ${request.method} Not Allowed`);
     }
   } catch (error) {
     console.log("Error");
@@ -32,15 +36,49 @@ async function getHandler(request, response) {
 }
 
 async function putHandler(request, response) {
-  const returnGuests = await guests.updateGuests(
-    request.query.id,
-    request.body,
-  );
+  try {
+    const returnGuest = await guests.getGuest(request.query.id);
 
-  return response.status(200).json(returnGuests[0]);
+    if (returnGuest.code === "22P02") {
+      return response.status(404).json({ message: "Invalid ID" });
+    }
+
+    if (returnGuest.length === 0) {
+      return response.status(404).json({ message: "Guest not found" });
+    }
+
+    if (request.body.id) {
+      return response.status(400).json({ message: "ID cannot be changed" });
+    }
+
+    if (request.body.name === "") {
+      return response.status(400).json({ message: "Name is required" });
+    }
+
+    if (returnGuest.length === 1) {
+      const returnGuests = await guests.updateGuests(
+        request.query.id,
+        request.body,
+      );
+
+      return response.status(200).json(returnGuests[0]);
+    }
+  } catch (error) {
+    return response.status(404).json(error);
+  }
 }
 
 async function deleteHandler(request, response) {
+  const returnGuest = await guests.getGuest(request.query.id);
+
+  if (returnGuest.code === "22P02") {
+    return response.status(404).json({ message: "Invalid ID" });
+  }
+
+  if (returnGuest.length === 0) {
+    return response.status(404).json({ message: "Guest not found" });
+  }
+
   const returnGuests = await guests.deleteGuests(request.query.id);
   return response.status(204).json(returnGuests[0]);
 }
