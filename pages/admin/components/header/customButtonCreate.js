db77348b4cf8792bedf35487d837569bf4b26a4d
Ryan Gwalchmei <ryan.gwalchmei@outlook.com.br>
2025-05-09 14:41:55 -0300

    feat: adds `export invitation button` in `admin` page

--- pages/admin/components/header/customButtonCreate.js
+++ pages/admin/components/header/customButtonCreate.js
@@ -1,26 +1,39 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
-import { FaPlus } from "react-icons/fa";
+import { FaDownload, FaPlus } from "react-icons/fa";
 import { useMenu } from "contexts/menuContext";
 import { useCreateData } from "contexts/createDataContext";
+import { useData } from "contexts/getDataContext";
 
 export default function CustomButtonCreate() {
   const { selectedMenu } = useMenu();
+  const { exportInvitations } = useData();
   const { invitations } = useCreateData();
 
   const { createNewInvitation } = invitations;
 
   function ButtonWhereInvitation() {
     return (
-      <Button
-        variant="outlined"
-        size="small"
-        startIcon={<FaPlus fontSize="small" />}
-        sx={{ minWidth: "fit-content" }}
-        onClick={() => createNewInvitation()}
-      >
-        Criar um novo Convite
-      </Button>
+      <>
+        <Button
+          variant="outlined"
+          size="small"
+          startIcon={<FaPlus fontSize="small" />}
+          sx={{ minWidth: "fit-content" }}
+          onClick={() => createNewInvitation()}
+        >
+          Criar um novo Convite
+        </Button>
+        <Button
+          variant="outlined"
+          size="small"
+          startIcon={<FaDownload fontSize="small" />}
+          sx={{ minWidth: "fit-content" }}
+          onClick={() => exportInvitations()}
+        >
+          Exportar Convites
+        </Button>
+      </>
     );
   }
 
