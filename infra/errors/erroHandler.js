926945e7ffb5fd52c3eba496896bf066f3da0e0c
Ryan Gwalchmei <ryan.gwalchmei@outlook.com.br>
2025-05-16 17:21:11 -0300

    refactor: refactor `erroHandler`

--- infra/errors/erroHandler.js
+++ infra/errors/erroHandler.js
@@ -8,30 +8,24 @@ import {
   ConflictError,
 } from "infra/errors/errors";
 
+const simpleHandledErrors = [
+  BadRequestError,
+  UnauthorizedError,
+  ForbiddenError,
+  ConflictError,
+  NotFoundError,
+];
+
 export function handleError(error, request, response) {
   if (error instanceof MethodNotAllowedError) {
     response.setHeader("Allow", error.allowedMethods);
     return response.status(error.statusCode).json(error.toJSON());
   }
 
-  if (error instanceof BadRequestError) {
-    return response.status(error.statusCode).json(error.toJSON());
-  }
-
-  if (error instanceof UnauthorizedError) {
-    return response.status(error.statusCode).json(error.toJSON());
-  }
-
-  if (error instanceof ForbiddenError) {
-    return response.status(error.statusCode).json(error.toJSON());
-  }
-
-  if (error instanceof ConflictError) {
-    return response.status(error.statusCode).json(error.toJSON());
-  }
-
-  if (error instanceof NotFoundError) {
-    return response.status(error.statusCode).json(error.toJSON());
+  for (const ErrorType of simpleHandledErrors) {
+    if (error instanceof ErrorType) {
+      return response.status(error.statusCode).json(error.toJSON());
+    }
   }
 
   const internalError = new InternalServerError({ cause: error });
